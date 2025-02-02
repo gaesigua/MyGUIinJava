bee1a1b3096580315396c3bdc89ee2f1fd1fcb65
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-02-02 20:22:53 +0200

    feb

--- src/GUI.java
+++ src/GUI.java
@@ -1,27 +1,49 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 public class GUI {
 	public static void main(String[] args) {
 		
-		JFrame myFrame = new JFrame();
-		JPanel myPanel = new JPanel();
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
 
 		
-		myFrame.setSize(400,200);
-		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.setVisible(true);
-		
-		myFrame.add(myPanel);
+		frame.setSize(500,300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(panel);
 		
 		
-		myPanel.setLayout(null);
+		panel.setLayout(null);
 		
-		JLabel label = new JLabel("Username");
-		label.setBounds(10, 20, 80, 25);
+		JLabel userNameLabel = new JLabel("Username");
+		userNameLabel.setBounds(10, 20, 80, 25);
+		panel.add(userNameLabel);
 		
-		myPanel.add(label);
+		
+		JTextField usernameText = new JTextField(20);
+		usernameText.setBounds(100, 20, 165, 25);
+		panel.add(usernameText);
+		
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setBounds(10, 50, 80, 25);
+		panel.add(passwordLabel);
+		
+		JPasswordField passwordText = new JPasswordField();
+		passwordText.setBounds(100, 50, 165, 25);
+		panel.add(passwordText);
+		
+		
+		JButton loginButton = new JButton();
+		loginButton.setBounds(10, 80, 80, 25);
+		panel.add(loginButton);
+		
+		frame.setVisible(true);
+		
+		
 		
 		
 		
