6f5e6a5218617b4c4cb2fce701088bfc644c8ca7
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-02-04 23:19:17 +0200

    feb4

--- src/GUI.java
+++ src/GUI.java
@@ -1,3 +1,6 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -5,10 +8,20 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class GUI {
+public class GUI implements ActionListener{
+	
+	
+	static JFrame frame;
+	static JLabel userNameLabel;
+	static JTextField usernameText;
+	static JLabel passwordLabel;
+	static JPasswordField passwordText;
+	static JButton loginButton;
+	static JLabel successLogin;
+	
 	public static void main(String[] args) {
 		
-		JFrame frame = new JFrame();
+		frame = new JFrame();
 		JPanel panel = new JPanel();
 
 		
@@ -19,33 +32,43 @@ public class GUI {
 		
 		panel.setLayout(null);
 		
-		JLabel userNameLabel = new JLabel("Username");
+		userNameLabel = new JLabel("Username");
 		userNameLabel.setBounds(10, 20, 80, 25);
 		panel.add(userNameLabel);
 		
 		
-		JTextField usernameText = new JTextField(20);
+		usernameText = new JTextField(20);
 		usernameText.setBounds(100, 20, 165, 25);
 		panel.add(usernameText);
 		
-		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel = new JLabel("Password");
 		passwordLabel.setBounds(10, 50, 80, 25);
 		panel.add(passwordLabel);
 		
-		JPasswordField passwordText = new JPasswordField();
+		passwordText = new JPasswordField();
 		passwordText.setBounds(100, 50, 165, 25);
 		panel.add(passwordText);
 		
 		
-		JButton loginButton = new JButton();
+		loginButton = new JButton("Login");
 		loginButton.setBounds(10, 80, 80, 25);
 		panel.add(loginButton);
+		loginButton.addActionListener(new GUI());
+		
+		successLogin = new JLabel("");
+		successLogin.setBounds(10, 110, 300, 25);
+		panel.add(successLogin);
+		
 		
 		frame.setVisible(true);
 		
 		
 		
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		
 		
 	}
