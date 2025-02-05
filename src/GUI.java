95f14b6d99d5e877b839d9446cf3684a0e7241ab
gaesigua <67262987+gaesigua@users.noreply.github.com>
2025-02-05 20:57:35 +0200

    feb5

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -71,62 +70,19 @@ public class GUI implements ActionListener{
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+		String user = usernameText.getText();
+		String password = passwordText.getText();
+//		System.out.println(user + ", " + password);
 		
-	}
-
-}
-=======
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
-
-public class GUI {
-	public static void main(String[] args) {
-		
-		JFrame frame = new JFrame();
-		JPanel panel = new JPanel();
-
-		
-		frame.setSize(500,300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(panel);
-		
-		
-		panel.setLayout(null);
-		
-		JLabel userNameLabel = new JLabel("Username");
-		userNameLabel.setBounds(10, 20, 80, 25);
-		panel.add(userNameLabel);
-		
-		
-		JTextField usernameText = new JTextField(20);
-		usernameText.setBounds(100, 20, 165, 25);
-		panel.add(usernameText);
-		
-		JLabel passwordLabel = new JLabel("Password");
-		passwordLabel.setBounds(10, 50, 80, 25);
-		panel.add(passwordLabel);
-		
-		JPasswordField passwordText = new JPasswordField();
-		passwordText.setBounds(100, 50, 165, 25);
-		panel.add(passwordText);
-		
-		
-		JButton loginButton = new JButton();
-		loginButton.setBounds(10, 80, 80, 25);
-		panel.add(loginButton);
-		
-		frame.setVisible(true);
-		
-		
-		
-		
+		if(user.equals("gaesigua") && (password.equals("abc123"))) {
+			
+			successLogin.setText("Login Successful!!!!!!!");
+			
+		}else {
+			System.out.println("Wrong Username or Password");
+		}
 		
 		
 	}
 
 }
->>>>>>> 4d7e01e7c825742b4178da48cc4ba878dcef951e
